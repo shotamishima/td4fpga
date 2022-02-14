bee1f471cb4be25d988d96db2a1b658ce4811c63
shotamishima <shota_mishima@toyota-tokyo.tech>
2022-02-14 10:15:51 +0900

    change to always

--- src/test_decoder.v
+++ src/test_decoder.v
@@ -3,6 +3,7 @@
 module top;
     parameter STEP = 10;
 
+    /* decodder module port */
     reg [7:0] op;
     reg carry_flag;
     wire [1:0] sel;
@@ -10,14 +11,38 @@ module top;
 
     decoder decoder_instance(.op(op), .carry_flag(carry_flag), .sel(sel), .load(load));
 
+    reg clk;
+
     initial begin
         $dumpfile("test_decoder.vcd");
         $dumpvars(0, decoder_instance);
-        
+
         carry_flag <= 0;
-        op <= 8'b00110011;
-    #STEP 
-        $display("op: %b, sel: %b, load: %b", op, sel, load);
+        op <= 8'h0f;
+        
+        /* finish after 5 micro sec */
+        #5000 $finish;
     end
+
+    always begin
+        clk = 1'b1;
+        #20;
+        clk = 1'b0;
+        #20;
+    end
+    
+    always @(posedge clk) begin
+        op <= op + 8'h10;
+        $display("op: %b sel: %b load: %b", op, sel, load);
+    end
+    /* initial begin */
+    /*     $dumpfile("test_decoder.vcd"); */
+    /*     $dumpvars(0, decoder_instance); */
+        
+    /*     carry_flag <= 0; */
+    /*     op <= 8'b00110011; */
+    /* #STEP */ 
+    /*     $display("op: %b, sel: %b, load: %b", op, sel, load); */
+    /* end */
 endmodule
     
